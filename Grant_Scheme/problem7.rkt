38eb98e9b637d0270ff6a95673ed0178b3778487
Grant Crofton <grantac@hotmail.com>
2015-04-01 20:49:00 +0100

    did problem 7

--- Grant_Scheme/problem7.rkt
+++ Grant_Scheme/problem7.rkt
@@ -7,3 +7,12 @@
 
 ; What is the 10 001st prime number?
 
+(define (nth-prime n)
+  (define (find-primes current primes)
+    (cond
+      ((= (length primes) n) (car primes))
+      ((divisible-by-any? current primes) (find-primes (add1 current) primes))
+      (else (find-primes (add1 current) (cons current primes)))))
+  (find-primes 2 '(3)))
+
+(nth-prime 10001)
